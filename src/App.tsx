c399a6cbfccaa7cf17ccd4d7d67c45342a95b6d0
sajankota <sajankota001@gmail.com>
2022-08-31 21:29:24 +0530

    removed unneccasy files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DashboardHomePage from './components/DashboardHomePage';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DashboardHomePage></DashboardHomePage>
+      <LandingPage></LandingPage>
+    </>
   );
 }
 
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,10 +1,8 @@
-import DashboardHomePage from "./DashboardHomePage"
 
 function LandingPage() {
     return (
         <>
             <h1>Landing Page</h1>
-            <DashboardHomePage></DashboardHomePage>
         </>
     )
 }
