443c91a5afaf0d57aaa544f81765195d250daa1f
sajankota <sajankota001@gmail.com>
2022-09-01 03:13:10 +0530

    changed the component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import DashboardHomePage from './components/DashboardHomePage';
 import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <>
-      <DashboardHomePage></DashboardHomePage>
       <LandingPage></LandingPage>
     </>
   );
